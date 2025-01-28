7d9a510e74375a11c8c964ffdad7114dd5b328a8
Arvind Dwarakanath <arvindd25@gmail.com>
2025-01-28 13:53:50 -0800

    generate all parantheses that are valid

---
+++ parantheses.py
@@ -0,0 +1,28 @@
+# generate all parantheses that are valid
+def generateParantheses(n: int) -> list:
+    res, stack = [], []
+
+    def backtrack(openB: int, closeB: int):
+    	# number of opened and closed parans = n
+        if openB == closeB == n:
+            res.append("".join(stack))
+
+        # we require more open parans
+        if openB < n:
+        	stack.append("(")
+        	backtrack(openB+1, closeB)
+        	stack.pop()
+            # remove the above added paran
+
+        # keep adding close parans till <= open
+        if closeB < openB:
+        	stack.append(")")
+        	backtrack(openB, closeB+1)
+        	stack.pop()
+
+    backtrack(0, 0)
+    return res
+
+print(generateParantheses(3))
+print(generateParantheses(2))
+print(generateParantheses(5))
