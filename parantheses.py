5e6fad7af232f9f299e30e2f64837d65b6a50418
Arvind Dwarakanath <arvindd25@gmail.com>
2025-02-08 10:43:16 -0800

    Inconsistent tabs and spaces are fixed in parantheses

--- parantheses.py
+++ parantheses.py
@@ -3,22 +3,22 @@ def generateParantheses(n: int) -> list:
     res, stack = [], []
 
     def backtrack(openB: int, closeB: int):
-    	# number of opened and closed parans = n
+        # number of opened and closed parans = n
         if openB == closeB == n:
             res.append("".join(stack))
 
         # we require more open parans
         if openB < n:
-        	stack.append("(")
-        	backtrack(openB+1, closeB)
-        	stack.pop()
+            stack.append("(")
+            backtrack(openB+1, closeB)
+            stack.pop()
             # remove the above added paran
 
         # keep adding close parans till <= open
         if closeB < openB:
-        	stack.append(")")
-        	backtrack(openB, closeB+1)
-        	stack.pop()
+            stack.append(")")
+            backtrack(openB, closeB+1)
+            stack.pop()
 
     backtrack(0, 0)
     return res
