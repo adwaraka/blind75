55eae84c084ef3670b96aea90341c7d48a49c992
Arvind Dwarakanath <arvindd25@gmail.com>
2025-07-18 15:20:04 -0700

    Add the problem statement and additional tests

--- interleaved.py
+++ interleaved.py
@@ -1,3 +1,12 @@
+"""
+Give three strings s1, s2 and s3, determine if s3 is formed by interleaving s1 and s2.
+A string s3 is an interleaving of s1 and s2 if:
+
+It contains all characters of s1 and s2 while preserving their relative order.
+Characters from s1 and s2 appear in s3 in the same order as in their original strings.
+The length of s3 equals the combined length of s1 and s2.
+"""
+
 def isInterleaved(s1: str, s2: str, s3: str) -> bool:
     # do the length of the two strings add up to the 3rd?
     if len(s3) != len(s1) + len(s2):
@@ -23,3 +32,5 @@ def isInterleaved(s1: str, s2: str, s3: str) -> bool:
 
 print(isInterleaved("abc", "xyz", "axbycz"))
 print(isInterleaved("abcd", "xyz", "xaybzcd"))
+print(isInterleaved("abc", "xyz", "bxaycz"))
+print(isInterleaved("abc", "xyz", "axbyzc"))
