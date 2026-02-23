5e96b6a6535ef8ed709c13b15a6c3cf843ac9de5
Arvind Dwarakanath <arvindd25@gmail.com>
2026-02-23 11:26:08 -0800

    BL-45: Wrote Jump 2 solution

--- jump2.py
+++ jump2.py
@@ -1,6 +1,16 @@
+# What are the minimum jumps required to hit the end?
+# farthest you can go; reach = max(reach, nums[i])
+
 def minJumps(nums: list) -> int:
-    # TODO
-    result = 0
+    # result is number of jumps
+    # reach is the maximum we can jump so far
+    # last indicates the maximum reachable index
+    result, reach, last = 0, 0, 0
+    for i in range(len(nums)-1):
+        reach = max(reach, i + nums[i])
+        if i == last:
+            last = reach
+            result+=1
     return result
 
 nums = [2, 3, 1, 1, 4]
